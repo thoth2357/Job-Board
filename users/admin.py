14ff81f974957c56290adda5c159a823e95aa617
kavyabhargava <68629867+kavyabhargava@users.noreply.github.com>
2022-07-24 04:34:41 +0530

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Resume
+from .models import Resume, Education
 # Register your models here.
 
 admin.site.register(Resume)
+admin.site.register(Education)
