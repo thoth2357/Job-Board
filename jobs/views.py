2ab316ba39e5cbecf1f3a2baaf5f0a4e15b3bf92
kavyabhargava <68629867+kavyabhargava@users.noreply.github.com>
2022-08-10 01:55:39 +0530

    Update views.py

--- jobs/views.py
+++ jobs/views.py
@@ -5,4 +5,15 @@ from .models import Jobs
 # Create your views here. #It tells django what html file to display
 def home(request):
     job_list = Jobs.objects.all() #import jobs from models and push it to front end
+    job_list = job_list[:4] #will display 4 jobs
     return render(request, 'home.html', {'jobs' : job_list}) 
+
+def job_list(request):
+    job_list = Jobs.objects.all() #import jobs from models and push it to front end
+    job_list = job_list[:20] #will display 4 jobs
+    return render(request, 'job-list.html', {'jobs' : job_list}) #make job-list.html
+
+def job_detail(request, slug): #will take request and slug(to identify which job which it is)
+   the_job = Jobs.objects.get(slug = slug) #getting job with that slug
+   return render(request, 'job-detail.html',{'object': the_job}) 
+    
