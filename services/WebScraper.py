60bf9f06a38839dde226c2c8568a8c5011637335
Thoth <98170427+thoth2357@users.noreply.github.com>
2022-08-12 14:06:31 +0100

    set up selenium and it options

--- services/WebScraper.py
+++ services/WebScraper.py
@@ -1,24 +1,27 @@
 #importing modules
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from fake_useragent import UserAgent
 
 
 
 class Scraper():
     def __init__(self) -> None:
-        "constructor method"
-        REQUEST_HEADERS = {
-                'pragma': 'no-cache', 
-                'cache-control': 'no-cache',
-                'sec-ch-ua': '" Not;A Brand";v="99", "Google Chrome";v="91", "Chromium";v="91"',
-                'sec-ch-ua-mobile': '?0',
-                'upgrade-insecure-requests': '1',
-                'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36',
-                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                'sec-fetch-site': 'none',
-                'sec-fetch-mode': 'navigate',
-                'sec-fetch-user': '?1',
-                'sec-fetch-dest': 'document',
-                'accept-language': 'en-US,en;q=0.9',
-            }
-        
+        #chrome option
+        self.chrome_options = Options()
+        self.chrome_options.add_argument("--window-size=1920,1080")
+        self.chrome_options.headless = True
+        self.chrome_options.add_argument("--disable-dev-shm-usage")
+        self.chrome_options.add_argument("--no-sandbox")
+        prefs = {"profile.managed_default_content_settings.images": 2}
+        self.chrome_options.add_experimental_option("prefs", prefs)
+        self.ua = UserAgent(use_cache_server=False)
+        self.userAgent = self.ua.random
+        self.chrome_options.add_argument(f'user-agent={self.userAgent}')
+        self.URL = ""
+    def get_driver_headless(self):
+        return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=self.chrome_options)
+    
+    def 
