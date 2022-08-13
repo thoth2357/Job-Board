51a7faebca87d1413e303307c46246acf30d65f7
Thoth <98170427+thoth2357@users.noreply.github.com>
2022-08-13 02:49:24 +0100

    scraping model done

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-from .models import 
+from .models import Scraping_Service
+
 # Register your models here.
+admin.site.register(Scraping_Service) # creates model in the admin
--- services/models.py
+++ services/models.py
@@ -2,10 +2,11 @@ from typing_extensions import Self
 from django.db import models
 
 # Create your models here.
-class Scraping_Services(models.Model):
+class Scraping_Service(models.Model):
     url_link = models.URLField(max_length=200, null=True, blank=True)
-
+    is_active = models.BooleanField(default=True)
+    
     def __str__(self):
         return self.url_link
     
-    
+
