c990f1864c3e4234ffd334c5026bb8a4a501d60a
Thoth <98170427+thoth2357@users.noreply.github.com>
2022-08-13 01:57:52 +0100

    configured periodic tasks on admin dash board

--- services/models.py
+++ services/models.py
@@ -1,3 +1,12 @@
+from typing_extensions import Self
 from django.db import models
 
 # Create your models here.
+class Scraping_Services(models.Model):
+    url_link = models.URLField(max_length=200, null=True, blank=True)
+    cron_job_time = models.TimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.url_link
+    
+    
