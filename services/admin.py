0a1b6ece8e09fd77d63a0841d62edfbb475cc0d0
Thoth <98170427+thoth2357@users.noreply.github.com>
2022-08-13 02:53:52 +0100

    model admin created and admin page with scraping model

--- services/admin.py
+++ services/admin.py
@@ -2,4 +2,22 @@ from django.contrib import admin
 from .models import Scraping_Service
 
 # Register your models here.
-admin.site.register(Scraping_Service) # creates model in the admin
+# admin.site.register(Scraping_Services) # creates model in the admin
+
+
+@admin.register(Scraping_Service)
+class Scraping_ServiceAdmin(admin.ModelAdmin):
+    list_display = ('url_link', 'is_active')
+    list_filter = ('is_active',)
+    search_fields = ('url_link',)
+    ordering = ('url_link',)
+    actions = ['make_active', 'make_inactive']
+
+    def make_active(self, request, queryset):
+        queryset.update(is_active=True)
+
+    def make_inactive(self, request, queryset):
+        queryset.update(is_active=False)
+    make_active.short_description = "Mark selected services as active"
+    make_inactive.short_description = "Mark selected services as inactive"
+    actions = [make_active, make_inactive]
