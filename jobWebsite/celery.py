989d8969bfebdf8b6e9addcfa20a757f391bbc89
thoth2357 <thoth2357@gmail.com>
2022-08-21 11:18:13 +0100

    little changes

--- jobWebsite/celery.py
+++ jobWebsite/celery.py
@@ -1,27 +1,30 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+# from services.tasks import Scraper
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jobWebsite.settings')
 
-app = Celery('jobWebsite')
-
-# Using a string here means the worker doesn't have to serialize
-# the configuration object to child processes.
-# - namespace='CELERY' means all celery-related configuration keys
-#   should have a `CELERY_` prefix.
-
+app = Celery('jobWebsite') 
 app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks() # Load task modules from all registered Django app configs.
 
+# Scraper = Scraper()
 
-# Load task modules from all registered Django app configs.
-app.autodiscover_tasks()
 
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
 
-@app.task(name='scrape_website')
-def call_name():
-    print("hello world")
+@app.task(name='Scraping_Service Task For Indeed.com')
+def start_scrapping_service_indeed(url_link):
+    #call function to start scraper from tasks
+    print('we are in indeed')
+    pass
+
+@app.task(name='Scraping_Service Task For LinkedIn.com')
+def start_scrapping_service_linkedin(url_link):
+    #call function to start scraper from tasks
+    print('we are in linkedin')
+    pass
--- services/tasks.py
+++ services/tasks.py
@@ -10,6 +10,7 @@ from fake_useragent import UserAgent
 
 #importing model from models.py
 from .models import Scraping_Service
+from jobWebsite.celery import start_scrapping_service_indeed, start_scrapping_service_linkedin
 
 class Scraper():
     def __init__(self) -> None:
@@ -24,26 +25,40 @@ class Scraper():
         self.ua = UserAgent(use_cache_server=False)
         self.userAgent = self.ua.random
         self.chrome_options.add_argument(f'user-agent={self.userAgent}')
+
+        self.get_url_links_from_db()
+
     def get_driver_headless(self):
         """
         Returns a webdriver object
         """
-        return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=self.chrome_options)
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=self.chrome_options)
+        return self.driver
     
-    def get_page_source(self, driver, URL):
+    def get_page_source(self, url):
         """
         Returns the page source of the webpage
         """
-        driver.get(URL)
-        return driver.page_source
+        self.driver.get(url)
+        return self.driver.page_source
 
-def get_url_links_from_db():
-    """
-    Returns all the URLs from the database
-    """
-    return [link for link in Scraping_Service.objects.all()]
+    def get_url_links_from_db(self):
+        """
+        Returns all the URLs from the database
+        """
+        for link in Scraping_Service.objects.all():
+            if link.is_active:
+                if link.url_link.split('.')[1] == 'indeed':
+                    start_scrapping_service_indeed.delay(link.url_link)
+                elif link.url_link.split('.')[1] == 'linkedin':
+                    start_scrapping_service_linkedin.delay(link.url_link)
+                else:
+                    #add logger here
+                    continue
+            else:
+                continue
 
-def start_web_scraping():
+def start_web_scraping_indeed():
     Scraper = Scraper()
     driver = Scraper.get_driver_headless()
-    page_source = Scraper.get_page_source(driver, 'https://giphy.com/login')
+    page_source = Scraper.get_page_source(driver, '')
