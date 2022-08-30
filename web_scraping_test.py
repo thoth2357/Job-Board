aaf77ce34abb45db72c3690130a5cadc67756185
thoth2357 <thoth2357@gmail.com>
2022-08-30 18:22:30 +0100

    testing cloudscraper tests in test.py

--- web_scraping_test.py
+++ web_scraping_test.py
@@ -1,1 +1,14 @@
-import requests
+from pprint import pprint
+import cloudscraper
+from bs4 import BeautifulSoup as beauty
+
+url = 'https://in.indeed.com/jobs?q=mckinsey&start=0&vjk=9346afd4052633e7'
+
+
+scraper = cloudscraper.create_scraper(delay=10, browser='chrome') 
+info  = scraper.get(url).content
+soup = beauty(info, 'html.parser')
+# pprint(soup.prettify())
+
+
+
