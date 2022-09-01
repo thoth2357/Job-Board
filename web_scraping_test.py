cbfcff93e38e458b78a99c5ccafb7d2dbff3c8b0
thoth2357 <thoth2357@gmail.com>
2022-09-01 22:14:46 +0100

    indeeed scraping code snippet added

--- web_scraping_test.py
+++ web_scraping_test.py
@@ -15,3 +15,25 @@ jobs_card = soup.find_all('div', class_='job_seen_beacon') #get all jobs cards
 
 #get all job spans
 jobs_card_span = soup.find_all('span', class_='jobtitle')
+
+'''
+span 1 = job title  -> to find use that jobTitle exists in id
+span 2 = company name -> use span class of companyName
+span 3 = rating -> use span class of ratingNumber
+to get company location make use of div class of companyLocation in the loop
+date posted - > 
+for job snippet look for div class of job-snippet and get li under it for snippet
+this is done and tested code snippet is below
+for card in jobs_card:
+            v=card.find_all('span')
+            print(card.find("span",id=re.compile("^jobTitle-")))
+            print("\n")
+            print(card.find('span',class_ = "companyName"))
+            print("\n")
+            print(card.find('div',class_="companyLocation"))
+            print("\n")
+            print(card.find('span', class_="ratingNumber")['aria-label'])
+            print("\n")
+            print(card.find('div', class_="job-snippet").find('li'))
+            break
+'''
