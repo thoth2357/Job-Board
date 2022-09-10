b496265213dec7a3abf5afa97b9d66b040e407c7
thoth2357 <thoth2357@gmail.com>
2022-09-10 16:30:37 +0100

    improved on linkedin scrapping

--- services/tasks.py
+++ services/tasks.py
@@ -13,6 +13,7 @@ from jobs.models import Job
 from .models import Scraping_Service
 from bs4 import BeautifulSoup as beauty
 import cloudscraper
+import requests
 import re
 import logging
 
@@ -62,6 +63,7 @@ def start_web_scraping_indeed():
                         duties=job_duties,
                         contract_type=job_type,
                         url_link=url_link,
+                        source="Indeed",
                     )
                     job_entry.save()
                     logging.info(f"{job_entry.id} {job_title} has been added to the database")
@@ -70,4 +72,51 @@ def start_web_scraping_indeed():
 
 
 def start_web_scraping_linkedin():
-    pass
+    '''
+    function to use to start webscraping tasks
+    '''
+    for link in Scraping_Service.objects.all():
+        if link.is_active:
+            if link.url_link.split(".")[1] == "linkedin":
+                try:
+                    response = requests.get(link.url_link)
+                    soup = beauty(response.content, "html.parser")
+                    jobs = soup.find_all('div', class_='base-card relative w-full hover:no-underline focus:no-underline base-card--link base-search-card base-search-card--link job-search-card')
+                    for job in jobs:
+                        job_title = job.find('h3', class_='base-search-card__title').text.strip()
+                        job_company = job.find('h4', class_='base-search-card__subtitle').text.strip()
+                        job_location = job.find('span', class_='job-search-card__location').text.strip()
+                        job_link = job.find('a', class_='base-card__full-link')['href']
+
+                        response2 = requests.get(job_link) 
+                        soup2 = beauty(response2.content,'html.parser')
+
+                        requirement = soup2.find('div', class_='description__text description__text--rich').text
+                        qualifications_index = requirement.index('Qualifications')
+                        duties_index = requirement.index('What You\'ll Do')
+                        pager_index = requirement.index('Show more')
+                        qualifications = requirement[qualifications_index:duties_index]
+                        duties = requirement[duties_index:pager_index].strip()
+                        category = soup2.find_all('span',class_='description__job-criteria-text description__job-criteria-text--criteria')[2].text.strip()
+                        contract_type = soup2.find_all('span',class_='description__job-criteria-text description__job-criteria-text--criteria')[1].text.strip()
+
+                        job_entry = Job.objects.create(
+                            title=job_title,
+                            company=job_company,
+                            location=job_location,
+                            duties=duties,
+                            requirement=qualifications,
+                            category=category,
+                            contract_type=contract_type,
+                            url_link=job_link,
+                            source='LinkedIn'
+                        )
+                        job_entry.save()
+                        logging.info(f"{job_entry.id} {job_title} has been added to the database")
+                except Exception as e:
+                    logging.error(f"Error in getting link {e}")
+                    break
+
+
+
+
