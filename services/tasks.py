29eb1f9cdc3516dbf8888cb3730635dfddc0f2d2
thoth <thoth2357@gmail.com>
2022-09-19 12:58:18 -0400

    on final test , realized cloudscraper fails on cloudflare V2 detection, Switched libraries

--- services/tasks.py
+++ services/tasks.py
@@ -3,19 +3,18 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from fake_useragent import UserAgent
 
-# importing model from models.py
+from bs4 import BeautifulSoup as beauty
+import re
+import requests
+import logging
+
 from jobs.models import Job
 from .models import Scraping_Service
-from bs4 import BeautifulSoup as beauty
-import cloudscraper
-import requests
-import re
-import logging
 
 
 logging.basicConfig(
@@ -23,26 +22,29 @@ logging.basicConfig(
     filemode="w",
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 )
-
+chrome_options = Options()
+chrome_options.headless = False
 
 @shared_task
 def start_web_scraping_indeed():
-    scraper = cloudscraper.create_scraper(delay=10, browser="chrome")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     for link in Scraping_Service.objects.all():
         if link.is_active:
             if link.url_link.split(".")[1] == "indeed":
                 try:
-                    info = scraper.get(link.url_link).content
+                    driver.get(link.url_link)
+                    page_source = driver.page_source
                 except Exception:
                     logging.error("Error in getting link")
                     break
-                soup = beauty(info, "html.parser")
+                soup = beauty(page_source, 'html.parser')
                 jobs_card = soup.find_all(
                     "div", class_="job_seen_beacon"
                 )  # get all jobs cards
                 for card in jobs_card:
                     # v = card.find_all("span")
                     job_title = card.find("a", id=re.compile("^job_")).find("span").text
+                    company_logo = card.find
                     company_name = card.find("span", class_="companyName").text
                     company_location = card.find("div", class_="companyLocation").text
                     job_rating = card.find("span", class_="ratingNumber")["aria-label"]
@@ -62,7 +64,7 @@ def start_web_scraping_indeed():
                         rating=job_rating,
                         duties=job_duties,
                         contract_type=job_type,
-                        url_link=url_link,
+                        url_link=url_link,  
                         source="Indeed",
                     )
                     job_entry.save()
