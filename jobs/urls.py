97d9d5112dc37ce3e588cfab8f432cfe38405a1e
thoth <thoth2357@gmail.com>
2022-09-24 23:47:38 +0100

    job detail url path .name edit

--- jobs/urls.py
+++ jobs/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', jobs_views.home, name='home'),   #this is home function inside the views.py in the jobs folder
     path('contact',jobs_views.contact, name='contact'),
     path('jobs/', jobs_views.job_list, name='job-list'),
-    path('jobs/<slug:slug>', jobs_views.job_detail, name='job-detail'), #slug will help us identify the specific instance of job
+    path('jobs/<slug:slug>', jobs_views.job_detail, name='job_detail'), #slug will help us identify the specific instance of job
 ]
 
