8c35733606b62c8a6d2a7817bf86957520385abb
thoth <thoth2357@gmail.com>
2022-09-24 07:27:38 +0100

    added contact view

--- jobs/views.py
+++ jobs/views.py
@@ -2,14 +2,23 @@ from django.shortcuts import render
 from django.core.paginator import Paginator
 from .models import Job
 
+from .utils import get_user_location
+
 
 # Create your views here. #It tells django what html file to display
 def home(request):
+    user_country = get_user_location(request) #get user country based on ip-address
     job_list = Job.objects.all()  # import jobs from models and push it to front end
     job_list = job_list[:4]  # will display 4 jobs
     jobs_count = Job.objects.count()
-    return render(request, "home.html", {"jobs": job_list, "jobs_count": jobs_count})
+    return render(request, "home.html", {"jobs": job_list, "jobs_count": jobs_count, "user_country": user_country})
 
+def contact(request):
+    '''
+    View for contact page
+    '''
+    context = {}
+    return render(request, "contact.html", context)
 
 def job_list(request):
     job_list = Job.objects.all()  # import jobs from models and push it to front end
@@ -22,3 +31,4 @@ def job_detail(
 ):  # will take request and slug(to identify which job which it is)
     the_job = Job.objects.get(slug=slug)  # getting job with that slug
     return render(request, "job-detail.html", {"object": the_job})
+
