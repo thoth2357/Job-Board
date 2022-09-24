425a3c9530b2b0fbf8df0e2c4479b6ac60b15dad
thoth <thoth2357@gmail.com>
2022-09-24 07:29:22 +0100

    added contact url path to urls file

--- jobs/urls.py
+++ jobs/urls.py
@@ -2,7 +2,8 @@ from django.urls import path,include
 from jobs import views as jobs_views  #from the job application it is gonna import views
 
 urlpatterns = [
-    path('', jobs_views.home, name='home_page'),   #this is home function inside the views.py in the jobs folder
+    path('', jobs_views.home, name='home'),   #this is home function inside the views.py in the jobs folder
+    path('contact',jobs_views.contact, name='contact'),
     path('jobs/', jobs_views.job_list, name='job-list'),
     path('jobs/<slug:slug>', jobs_views.job_detail, name='job-detail'), #slug will help us identify the specific instance of job
 ]
