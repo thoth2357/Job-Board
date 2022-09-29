54806e395316af17d9a10d284387fc2fb799da49
thoth <thoth2357@gmail.com>
2022-09-29 17:30:46 +0100

    filter tags model created

--- services/models.py
+++ services/models.py
@@ -1,3 +1,4 @@
+from enum import unique
 from django.db import models
 
 # Create your models here.
@@ -7,3 +8,15 @@ class Scraping_Service(models.Model):
 
     def __str__(self):
         return self.url_link
+
+class Filter_tag(models.Model):
+    '''
+    model to use for filter tagging
+    '''
+    tag_name = models.CharField(max_length=25, null=True, blank=True, unique=True, help_text="Tag name to use for filtering. Must be unique.")
+    tag_description = models.TextField(max_length=100, null=True, blank=True)
+    tag_search_company = models.CharField(max_length=100, null=True, blank=True,help_text="Companies name to search for, separated by comma (,). Leave blank if interested in Category only.")
+    tag_search_category = models.CharField(max_length=100, null=True, blank=True, help_text="Categories to search for, separated by comma (,). Leave blank if interested in Company only.")
+
+    def __str__(self) -> str:
+        return f"{self.tag_name}"
