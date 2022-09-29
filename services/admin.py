f8a2a9f1ec1e8e29065d75db913e00a5c0e1130f
thoth <thoth2357@gmail.com>
2022-09-29 17:31:26 +0100

    filter tags places in admin and placeholder overridden

--- services/admin.py
+++ services/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Scraping_Service
+from .models import Scraping_Service, Filter_tag
 
 # Register your models here.
 # admin.site.register(Scraping_Services) # creates model in the admin
@@ -21,3 +21,18 @@ class Scraping_ServiceAdmin(admin.ModelAdmin):
     make_active.short_description = "Mark selected services as active"
     make_inactive.short_description = "Mark selected services as inactive"
     actions = [make_active, make_inactive]
+
+@admin.register(Filter_tag)
+class FilterTagAdmin(admin.ModelAdmin):
+    list_display = ("tag_name", "tag_description", "tag_search_company", "tag_search_category")
+    list_filter = ("tag_name",)
+    search_fields = ("tag_name", "tag_description", "tag_search_company", "tag_search_category")
+    ordering = ("tag_name",)
+
+    def render_change_form(self, request, context, *args, **kwargs):
+        form_instance = context['adminform'].form
+        form_instance.fields['tag_name'].widget.attrs['placeholder'] = 'E.g. GMStrat'
+        form_instance.fields['tag_description'].widget.attrs['placeholder'] = 'E.g. Tag For Finding Strategy Jobs in Google and Microsoft'
+        form_instance.fields['tag_search_company'].widget.attrs['placeholder'] = 'E.g. google and Microsoft'
+        form_instance.fields['tag_search_category'].widget.attrs['placeholder'] = 'E.g. Strategy'
+        return super().render_change_form(request, context, *args, **kwargs)
