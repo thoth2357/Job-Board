8c83b17ea5a2319b46c0fa86e469d663f5974e6f
thoth <thoth2357@gmail.com>
2022-10-01 18:32:59 +0100

    added search url and override saved method to create search url for filter tags

--- services/models.py
+++ services/models.py
@@ -17,6 +17,11 @@ class Filter_tag(models.Model):
     tag_description = models.TextField(max_length=100, null=True, blank=True)
     tag_search_company = models.CharField(max_length=100, null=True, blank=True,help_text="Companies name to search for, separated by comma (,). Leave blank if interested in Category only.")
     tag_search_category = models.CharField(max_length=100, null=True, blank=True, help_text="Categories to search for, separated by comma (,). Leave blank if interested in Company only.")
-
+    search_url = models.URLField(max_length=200, null=True, blank=True, help_text="Automatically generated..Readonly Field can't be edited")
     def __str__(self) -> str:
         return f"{self.tag_name}"
+    
+    def save(self, *args, **kwargs):
+        if self.tag_search_company is not None and self.tag_search_category is not None:
+            self.search_url = f"?q=job_company={self.tag_search_company.replace(',','')}&category={self.tag_search_category}"
+        super(Filter_tag, self).save(*args, **kwargs)
