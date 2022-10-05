ca65ec18561f2b393bc883b03d817d80982be1cf
thoth <thoth2357@gmail.com>
2022-10-05 12:26:15 +0100

    added location icontains

--- jobs/filters.py
+++ jobs/filters.py
@@ -9,6 +9,7 @@ from django import forms
 
 class JobsFilter(django_filters.FilterSet):
     job_company = django_filters.CharFilter(method='custom_filter')
+    location  = django_filters.CharFilter(method='custom_filter_location')
     # contract_type = django_filters.BooleanFilter(widget=forms.CheckboxInput)
     class Meta:
         model = Job
@@ -16,3 +17,5 @@ class JobsFilter(django_filters.FilterSet):
         
     def custom_filter(self, queryset, title, value):
         return queryset.filter(Q(title__icontains=value) | Q(company__icontains=value))
+    def custom_filter_location(self, queryset, title, value):
+        return queryset.filter(Q(location__icontains=value) | Q(location__icontains=value))
