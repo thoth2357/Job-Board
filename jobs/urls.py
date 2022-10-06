3bf28dce9a3e512fee2ad1bed1fa27c54bb9ddbb
thoth <thoth2357@gmail.com>
2022-10-06 23:33:04 +0100

    FIX:removed url that wasnt in use anymore

--- jobs/urls.py
+++ jobs/urls.py
@@ -4,7 +4,7 @@ from jobs import views as jobs_views  #from the job application it is gonna impo
 urlpatterns = [
     path('', jobs_views.home, name='home'),   #this is home function inside the views.py in the jobs folder
     path('contact',jobs_views.contact, name='contact'),
-    path('jobs/', jobs_views.job_list, name='job-list'),
+    # path('jobs/', jobs_views.job_list, name='job-list'),
     path('jobs/<slug:slug>', jobs_views.job_detail, name='job_detail'), #slug will help us identify the specific instance of job
 
     path('jobs-search/', jobs_views.job_search, name='job_search'),
