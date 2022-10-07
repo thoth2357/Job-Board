603a7b3c4e3714d9ab19c7162deb6440d19658f6
thoth <thoth2357@gmail.com>
2022-10-07 08:23:15 +0100

    HACK: Added category field to filter tag

--- jobs/filters.py
+++ jobs/filters.py
@@ -10,6 +10,7 @@ from django import forms
 class JobsFilter(django_filters.FilterSet):
     job_company = django_filters.CharFilter(method='custom_filter')
     location  = django_filters.CharFilter(method='custom_filter_location')
+    category = django_filters.CharFilter(method='custom_filter_category')
     # contract_type = django_filters.BooleanFilter(widget=forms.CheckboxInput)
     class Meta:
         model = Job
@@ -19,3 +20,6 @@ class JobsFilter(django_filters.FilterSet):
         return queryset.filter(Q(title__icontains=value) | Q(company__icontains=value))
     def custom_filter_location(self, queryset, title, value):
         return queryset.filter(Q(location__icontains=value) | Q(location__icontains=value))
+    def custom_filter_category(self, queryset, title, value):
+        return queryset.filter(Q(category__icontains=value) | Q(category__icontains=value))
+    
