910c4aed099084a64f4637c40a5cf10f14e76216
thoth <thoth2357@gmail.com>
2022-10-12 19:26:33 +0100

    ADD: added more filter options to admin page

--- jobs/admin.py
+++ jobs/admin.py
@@ -3,6 +3,6 @@ from .models import Job
 
 @admin.register(Job)
 class JobAdmin(admin.ModelAdmin):
-    list_display =  ("title", "company", "location", "category", "date_posted") 
-    list_filter =  ("company", "category", "date_posted") 
+    list_display =  ("title", "company", "location", "category", "date_posted","contract_type") 
+    list_filter =  ("company", "category", "date_posted", "source") 
     search_fields = ("title", "company", "location", "category", "date_posted") 
