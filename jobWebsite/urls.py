67f6525cdaab8cb0ae5a8bebfc9e09c3ee557780
Thoth <thoth2357@gmail.com>
2022-10-16 22:14:11 +0100

    removed auth routes from main project urls to user urls

--- jobWebsite/urls.py
+++ jobWebsite/urls.py
@@ -16,22 +16,14 @@ Including another URLconf
 from re import template
 from django.contrib import admin
 from django.urls import path,include
-from users import views as users_views
-from django.contrib.auth import views as auth_views
 
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', users_views.register, name='signup-login_page'), #if someone goes to /register they are gonna find it
-    path('users/create/', users_views.create_resume, name='create-resume'), #
-    path('users/view/<slug:slug>/', users_views.resume_detail, name='resume-detail'), #name shall be same as we have in reverse(), it is class based view
-    path('profile/', users_views.profile, name='profile'), #if someone goes to /register they are gonna find it
-    path('login/', auth_views.LoginView.as_view(template_name = 'login.html'), name= 'login'), #this will be class based view.For class based view, have to add as_view in the end
-    path('logout/', auth_views.LogoutView.as_view(template_name = 'logout.html'), name= 'logout'), 
-    path('logging-in/', users_views.login, name= 'logging'),
-    path('', include('jobs.urls'))
+    path('', include('jobs.urls')),
+    path('auth/', include('users.urls')),
 ]
 
 if settings.DEBUG:
