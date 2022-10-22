6f443d5a518d56b8dd27189212b394f2a2434506
Thoth <thoth2357@gmail.com>
2022-10-22 13:33:34 +0100

    add date posted scraping functionality for linkedin site

--- web_scraping_test.py
+++ web_scraping_test.py
@@ -1,6 +1,8 @@
 # from pprint import pprint
 from datetime import datetime, timedelta
+import re
 import requests
+import time
 # import cloudscraper
 # import re
 from bs4 import BeautifulSoup as beauty
@@ -72,48 +74,66 @@ url4 = 'https://in.indeed.com/jobs?q=mckinsey&start=0&vjk=d25cab40526ba5f6'
 # # print(jobs_card_span)
 
 # -----------------------------------------------------------------------------------------linkedin
-# response = requests.get(url2)
-# soup = beauty(response.content, "html.parser")
-# jobs = soup.find_all('div', class_='base-card relative w-full hover:no-underline focus:no-underline base-card--link base-search-card base-search-card--link job-search-card')
+response = requests.get(url2)
+soup = beauty(response.content, "html.parser")
+jobs = soup.find_all('div', class_='base-card relative w-full hover:no-underline focus:no-underline base-card--link base-search-card base-search-card--link job-search-card')
 # print(len(jobs))
-# for job in jobs:
-#     job_title = job.find('h3', class_='base-search-card__title').text.strip()
-#     job_company = job.find('h4', class_='base-search-card__subtitle').text.strip()
-#     job_location = job.find('span', class_='job-search-card__location').text.strip()
-#     job_link = job.find('a', class_='base-card__full-link')['href']
-#     job_company_logo = job.find('div', class_='search-entity-media').find('img')['data-delayed-url']
+for job in jobs:
+    job_title = job.find('h3', class_='base-search-card__title').text.strip()
+    job_company = job.find('h4', class_='base-search-card__subtitle').text.strip()
+    job_location = job.find('span', class_='job-search-card__location').text.strip()
+    job_link = job.find('a', class_='base-card__full-link')['href']
+    job_company_logo = job.find('div', class_='search-entity-media').find('img')['data-delayed-url']
 
-#     print(job_title)
-#     print(job_company)
-#     print(job_location)
-#     print(job_link)
-#     print(job_company_logo)
-#     print("\n")
+    print(job_title)
+    print(job_company)
+    print(job_location)
+    print(job_link)
+    print(job_company_logo)
+    print("\n")
 
-#     response2 = requests.get(job_link)
-#     # print(response2)
-#     soup2 = beauty(response2.content,'html.parser')
-#     # requirement = soup2.find('div', class_='description__text description__text--rich').find('ul').find_all('li')
-#     # requirement = [i.text for i in requirement]
-#     # print(requirement)
-
-#     requirement = soup2.find('div', class_='description__text description__text--rich').text
-#     qualifications_index = requirement.index('Qualifications')
-#     duties_index = requirement.index('What You\'ll Do')
-#     pager_index = requirement.index('Show more')
-#     qualifications = requirement[qualifications_index:duties_index]
-#     duties = requirement[duties_index:pager_index].strip()
-#     category = soup2.find_all('span',class_='description__job-criteria-text description__job-criteria-text--criteria')[2].text.strip()
-#     contract_type = soup2.find_all('span',class_='description__job-criteria-text description__job-criteria-text--criteria')[1].text.strip()
-#     print(qualifications)
-#     print('\n')
-#     print(duties)
-#     print('\n')
-#     print(contract_type)
-#     print('\n')
-#     print(category)
-#     print("\n")
-#     break
+    response2 = requests.get(job_link)
+    time.sleep(5)
+    
+    # print(response2)
+    soup2 = beauty(response2.content,'html.parser')
+    # requirement = soup2.find('div', class_='description__text description__text--rich').find('ul').find_all('li')
+    # requirement = [i.text for i in requirement]
+    # print(requirement)
+    
+    requirement = soup2.find('div', class_='description__text description__text--rich').text
+    qualifications_index = requirement.index('Qualifications')
+    duties_index = requirement.index('What You\'ll Do')
+    pager_index = requirement.index('Show more')
+    qualifications = requirement[qualifications_index:duties_index]
+    duties = requirement[duties_index:pager_index].strip()
+    category = soup2.find_all('span',class_='description__job-criteria-text description__job-criteria-text--criteria')[2].text.strip()
+    contract_type = soup2.find_all('span',class_='description__job-criteria-text description__job-criteria-text--criteria')[1].text.strip()
+    date_posted = soup2.find('span',class_='posted-time-ago__text topcard__flavor--metadata').text.strip()
+    # print('date_posted', date_posted)
+    if 'day' in date_posted:
+        timer = 1
+    elif 'week' in date_posted:
+        timer = 7
+    elif 'month' in date_posted:
+        timer = 30
+    elif 'year' in date_posted:
+        timer = 365
+    else:
+        timer = 1
+    count_of_time = int(re.findall(r'[0-9]', date_posted)[0])
+    total_time_elapsed = count_of_time * timer
+    date_job_posted_datetime = datetime.now() - timedelta(days=total_time_elapsed)
+    # print('date_job_posted_datetime', date_job_posted_datetime)
+    print(qualifications)
+    print('\n')
+    print(duties)
+    print('\n')
+    print(contract_type)
+    print('\n')
+    print(category)
+    print("\n")
+    break
 
 # -----------------------------------------------------------------------------------indeed
 # from selenium import webdriver
