0805cf4d9dbd90aecdacf798bbb0322f276311cc
Thoth <thoth2357@gmail.com>
2022-10-23 14:31:38 +0100

    registered role to admin page

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import Job
+from .models import Job, Role
 
 @admin.register(Job)
 class JobAdmin(admin.ModelAdmin):
     list_display =  ("title", "company", "location", "category", "date_posted","contract_type") 
     list_filter =  ("company", "category", "date_posted", "source") 
     search_fields = ("title", "company", "location", "category", "date_posted") 
+
+# @admin.register(Roles)
+# class RoleAdmin(admin.ModelAdmin):
+#     list_display = ("role")
+admin.site.register(Role)
+
+
