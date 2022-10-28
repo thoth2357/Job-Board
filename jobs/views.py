d22fb4650247b2645eea619c551d20c3c092c3db
Thoth <thoth2357@gmail.com>
2022-10-28 20:36:54 +0100

    FIX SCRAPER GENERALISM: increased pagination to 10 and conditional statements for job requirements in view

--- jobs/views.py
+++ jobs/views.py
@@ -40,7 +40,10 @@ def job_detail(request, slug:str):  # will take request and slug(to identify whi
     '''
     user_country = get_user_location(request) #get user country based on ip-address
     job = Job.objects.get(slug=slug)  # getting job with that slug
-    requirement = job.requirements.split("\n")
+    if job.requirements:
+        requirement = job.requirements.split("\n")
+    else:
+        requirement = None
     split_title = job.title.split(' ')
     related_jobs = [Job.objects.all().filter(title__icontains = i) for i in split_title]
     non_duplicate_related_jobs = set([job for i in related_jobs for job in i])
@@ -51,7 +54,7 @@ def job_search(request):
     jobs_listing = Job.objects.all()
     contract_types = Job.objects.values_list('contract_type', flat=True).distinct()
     listing_filter = JobsFilter(request.GET, queryset=jobs_listing)
-    paginated_listing_filter = Paginator(listing_filter.qs.order_by('-date_posted'), 4)
+    paginated_listing_filter = Paginator(listing_filter.qs.order_by('-date_posted'), 10)
     job_per_page = paginated_listing_filter.get_page(request.GET.get('page'))
     # .adjusted_elided_pages = paginated_listing_filter.get_elided_page_range(request.GET.get('page'))
     
